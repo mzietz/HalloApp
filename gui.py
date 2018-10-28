cc596809f10f226326b89586eed30089fb508ff8
mzietz <zietzmaximilian@gmail.com>
2018-10-28 22:43:20 +0100

    basics working

--- gui.py
+++ gui.py
@@ -3,11 +3,11 @@
 
 import sys
 from PyQt5.QtWidgets import (QWidget, QToolTip, 
-    QPushButton, QApplication, QLabel, QHBoxLayout, QVBoxLayout, QTableWidget, QTableWidgetItem)
+    QPushButton, QApplication, QLabel, QHBoxLayout, QVBoxLayout, QTableWidget, QTableWidgetItem, QLineEdit, QGridLayout)
 from PyQt5.QtGui import QFont 
 from PyQt5.QtCore import QProcess
 from library import *
-
+import random
 
 class Flashcards(QWidget):
     
@@ -15,7 +15,7 @@ class Flashcards(QWidget):
         super().__init__()        
         self.left = 0
         self.top = 0
-        self.width = 300
+        self.width = 400
         self.height = 200
         self.library = Library
         self.initUI()
@@ -26,30 +26,78 @@ class Flashcards(QWidget):
         self.layout = QHBoxLayout()
         self.setLayout(self.layout)
         self.deckWindow()
+        self.currentDeck = 0
+        self.currentCard = 0
+        self.addQuestion = ""
+        self.addAnswer = ""
 
     def deckWindow(self):
-       # Create table
         self.clear()
+        self.library.saveLibrary()
         self.layout.addStretch(1)
         self.tableWidget = QTableWidget()
         self.layout.addWidget(self.tableWidget)
         self.setLayout(self.layout)
         self.tableWidget.setRowCount(len(self.library.deck))
-        self.tableWidget.setColumnCount(1)
+        self.tableWidget.setColumnCount(3)
         d=0
         for x in self.library.deck:
             self.tableWidget.setItem(d,0, QTableWidgetItem(self.library.deck[d].name))
+            self.tableWidget.setItem(d,1, QTableWidgetItem("Learn"))
+            self.tableWidget.setItem(d,2, QTableWidgetItem("Add Card"))
             d+=1
         self.tableWidget.clicked.connect(self.on_click_deck)
 
     def on_click_deck(self):
-        print("\n")
+#        print("\n")
         for currentQTableWidgetItem in self.tableWidget.selectedItems():
-            print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
-            self.trainerWindow(0)
+#            print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
+            self.currentDeck = currentQTableWidgetItem.row()
+#            print(self.currentDeck)
+            if currentQTableWidgetItem.text() == "Add Card":
+                self.addCardWindow(self.currentDeck)
+            if currentQTableWidgetItem.text() == "Learn":
+                self.trainerWindow(self.currentDeck)
 
     def on_click_iknow(self):
         print("i Know")
+        self.layout.addWidget(self.answerWidget)
+        self.setLayout(self.layout)
+        self.nextButton = QPushButton("NExt")
+        self.layout.addWidget(self.nextButton)
+        source = self.sender()
+        if source.text() == "Keine Ahnung":
+            self.library.deck[self.currentDeck].card[0].learnedStatus=1                
+        elif source.text() == "Leise Ahnung":
+            self.library.deck[self.currentDeck].card[0].learnedStatus=2  
+        elif source.text() == "Gerade so":
+            self.library.deck[self.currentDeck].card[0].learnedStatus=3
+        elif source.text() == "Easy Peasy":
+            self.library.deck[self.currentDeck].card[0].learnedStatus=4
+        print(self.library.deck[self.currentDeck].card[0].learnedStatus)
+        self.nextButton.clicked.connect(self.on_click_next)
+
+    def on_click_next(self):
+        print("next")
+        self.trainerWindow(self.currentDeck)
+
+    def on_click_addCard(self):
+        print("add Card")
+        self.library.deck[self.currentDeck].addCard()
+        self.library.deck[self.currentDeck].card[-1].question = self.questionEdit.text()
+#        print("nummer: "+"text"+self.questionEdit.text())
+        self.library.deck[self.currentDeck].card[-1].answer = self.answerEdit.text()
+        self.library.deck[self.currentDeck].card[-1].timeUntilShow = 0
+        self.library.deck[self.currentDeck].card[-1].learnedStatus = 0
+        print("Q: " +self.library.deck[self.currentDeck].card[-1].question)
+        print("A: " +self.library.deck[self.currentDeck].card[-1].answer)
+        print("1: " +str(self.library.deck[self.currentDeck].card[-1].timeUntilShow))
+        print("2: " +str(self.library.deck[self.currentDeck].card[-1].learnedStatus))
+        self.addCardWindow(self.currentDeck)
+    
+    def on_click_cancel(self):
+        print("Cancel")
+        self.deckWindow()
 
     def clear(self):
         for i in reversed(range(self.layout.count())):
@@ -57,19 +105,52 @@ class Flashcards(QWidget):
                 self.layout.itemAt(i).widget().setParent(None)
             except:
                 pass
+#        print("Cleared "+str(self.layout.count()) +" objects")
 
     def trainerWindow(self, deck):
         self.clear()
-        print (deck)
-        self.iKnowButton = QPushButton("I Know This!")
+        print("trainer")
+        self.a1Button = QPushButton("Keine Ahnung")
+        self.a2Button = QPushButton("Leise Ahnung")
+        self.a3Button = QPushButton("Gerade so")
+        self.a4Button = QPushButton("Easy Peasy")
+        #self.currentCard = 0 
+        self.library.deck[self.currentDeck].sortDeck()
         self.questionWidget = QLabel(self.library.deck[deck].card[0].question)
         self.answerWidget = QLabel(self.library.deck[deck].card[0].answer)
         self.layout.addWidget(self.questionWidget)
-        self.layout.addWidget(self.answerWidget)
-        self.layout.addWidget(self.iKnowButton)
+        self.layout.addWidget(self.a1Button)
+        self.layout.addWidget(self.a2Button)
+        self.layout.addWidget(self.a3Button)
+        self.layout.addWidget(self.a4Button)
         self.setLayout(self.layout)
-        self.iKnowButton.clicked.connect(self.on_click_iknow)
+        self.a1Button.clicked.connect(self.on_click_iknow)
+        self.a2Button.clicked.connect(self.on_click_iknow)
+        self.a3Button.clicked.connect(self.on_click_iknow)
+        self.a4Button.clicked.connect(self.on_click_iknow)
+        self.cancelButton = QPushButton("Cancel!")
+        self.cancelButton.clicked.connect(self.on_click_cancel)
+        self.layout.addWidget(self.cancelButton)
 
+
+    def addCardWindow(self, deck):
+        self.clear()
+        question = QLabel('Question')
+        answer = QLabel('Answer')
+        self.questionEdit = QLineEdit()
+        self.answerEdit = QLineEdit()
+        self.layout.setSpacing(10)
+        self.layout.addWidget(question)
+        self.layout.addWidget(self.questionEdit)
+        self.layout.addWidget(answer)
+        self.layout.addWidget(self.answerEdit)
+        self.submitButton = QPushButton("Add CArd!")
+        self.submitButton.clicked.connect(self.on_click_addCard)
+        self.cancelButton = QPushButton("Cancel!")
+        self.cancelButton.clicked.connect(self.on_click_cancel)
+        self.layout.addWidget(self.submitButton)
+        self.layout.addWidget(self.cancelButton)
+        self.setLayout(self.layout)
 
 if __name__=="__main__":
 
--- library.py
+++ library.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from operator import itemgetter, attrgetter
 
 class Library:
 	def __init__(self):
@@ -26,23 +27,25 @@ class Library:
 					self.deck[d-1].card[c-1].answer = line[:end]
 					line = line[end+1:]
 					end = line.find(':')
-					self.deck[d-1].card[c-1].learnedStatus = line[:end]
+					self.deck[d-1].card[c-1].learnedStatus = int(line[:end])
 					line = line[end+1:]
 					end = line.find(':')
-					self.deck[d-1].card[c-1].timeUntilShow = line[:end]
+					self.deck[d-1].card[c-1].timeUntilShow = int(line[:end])
 
 	def saveLibrary(self):
+#		print(self.deck[0].card[0].question+":"+self.deck[0].card[0].answer+":"+self.deck[0].card[0].learnedStatus+":"+self.deck[0].card[0].timeUntilShow)
 		with open("library1.txt", "w") as f:
 			d=0 # counter for decks
 			for x1 in self.deck:
-				f.write("D:"+self.deck[d].name+":"+self.deck[d].number)
+				f.write("D!"+self.deck[d].name+":"+self.deck[d].number)
 				c=0 # counter for cards
 				for x2 in self.deck[d].card:
-					f.write(self.deck[d].card[c].question+":"+self.deck[d].card[c].answer+":"+self.deck[d].card[c].learnedStatus+":"+self.deck[d].card[c].timeUntilShow+"\n")
+					f.write(self.deck[d].card[c].question+":"+self.deck[d].card[c].answer+":"+str(self.deck[d].card[c].learnedStatus)+":"+str(self.deck[d].card[c].timeUntilShow)+"\n")
 #					print(self.deck[d].card[c].question+":"+self.deck[d].card[c].answer+":"+self.deck[d].card[c].learnedStatus+":"+self.deck[d].card[c].timeUntilShow)
 					c+=1
 				d+=1
 		f.close()
+		print("saved")
 	def giveName(self, name):
 		self.name = name
 	def addDeck(self, name):
@@ -57,7 +60,10 @@ class Deck:
 		self.name = name
 	def addCard(self):
 		self.card.append(Card())
-
+#		self.card[-1].question = question
+#		self.card[-1].answer = answer
+	def sortDeck(self):
+		self.card.sort(key=attrgetter('learnedStatus'))
 
 class Card:
 	def __init__ (self):
@@ -69,12 +75,18 @@ class Card:
 if __name__=="__main__":
 	myLibrary = Library()
 	myLibrary.loadLibrary()
+	print (myLibrary.deck[1].card[0].learnedStatus)
+	print (myLibrary.deck[1].card[1].learnedStatus)
+	print (myLibrary.deck[1].card[2].learnedStatus)
+	print (myLibrary.deck[1].card[3].learnedStatus)
+#	myLibrary.saveLibrary()
+	myLibrary.deck[1].sortDeck()
 	myLibrary.saveLibrary()
 #	print (myLibrary.deck[0].name)
 #	print (myLibrary.deck[1].name)
 #	print (myLibrary.deck[0].number)
 #	print (myLibrary.deck[1].number)
-#	print (myLibrary.deck[1].card[1].question)
-#	print (myLibrary.deck[1].card[1].answer)
-#	print (myLibrary.deck[1].card[1].learnedStatus)
-#	print (myLibrary.deck[1].card[1].timeUntilShow)
+	print (myLibrary.deck[1].card[0].learnedStatus)
+	print (myLibrary.deck[1].card[1].learnedStatus)
+	print (myLibrary.deck[1].card[2].learnedStatus)
+	print (myLibrary.deck[1].card[3].learnedStatus)
